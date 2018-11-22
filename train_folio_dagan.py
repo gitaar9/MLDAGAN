71cca99ba21d3638d6e51e2188e28c8bf30e6648
gitaar9 <thijs.eker@gmail.com>
2018-11-22 23:28:51 +0100

    added a comment explaining how to run the program

--- train_folio_dagan.py
+++ train_folio_dagan.py
@@ -2,6 +2,10 @@ import data as dataset
 from experiment_builder import ExperimentBuilder
 from utils.parser_util import get_args
 
+
+# Run this with:
+# python train_folio_dagan.py --batch_size 32 --generator_inner_layers 3 --discriminator_inner_layers 5 --num_generations 64 --experiment_title folio22112018 --num_of_gpus 1 --z_dim 100 --dropout_rate_value 0.5
+
 batch_size, num_gpus, args = get_args()
 #set the data provider to use for the experiment
 data = dataset.FolioDAGANDataset(batch_size=batch_size, last_training_class_index=900, reverse_channels=True,
